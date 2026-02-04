9bc76416e3f8d897e2f32d833a2c0904cfa1c9f1
PriyanshiRaina07 <priyanshiraina07@gmail.com>
2026-02-04 12:17:53 +0530

    Time: 175 ms (23.54%), Space: 298.4 MB (21.65%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,35 +1,74 @@
+// class Solution {
+//     public boolean validPath(int n, int[][] edges, int source, int destination) {
+//         ArrayList<ArrayList<Integer>>adj = new ArrayList<>();
+
+
+//         for(int i = 0; i < n; i++){
+//             adj.add(new ArrayList<>());
+//         }
+//         for(int[] edge:edges){
+//             int u = edge[0];
+//             int v = edge[1];
+//             adj.get(u).add(v);
+//             adj.get(v).add(u); 
+//         }
+//         boolean[] visited = new boolean[n];
+//         Queue<Integer> q = new LinkedList<>();
+
+//         q.add(source);
+//         visited[source] = true;
+
+//         while (!q.isEmpty()) {
+//             int curr = q.poll();
+//               if (curr == destination) return true;
+
+//             for (int neighbour : adj.get(curr)) {
+//                 if (!visited[neighbour]) {
+//                     visited[neighbour] = true;
+//                     q.add(neighbour);
+//                 }
+//             }
+        
+//         }
+//         return false;
+//     }
+
+    
+
+// }
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        ArrayList<ArrayList<Integer>>adj = new ArrayList<>();
 
 
-        for(int i = 0; i < n; i++){
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
-        for(int[] edge:edges){
+
+        for (int[] edge : edges) {
             int u = edge[0];
             int v = edge[1];
             adj.get(u).add(v);
-            adj.get(v).add(u); 
+            adj.get(v).add(u);
         }
+
         boolean[] visited = new boolean[n];
-        Queue<Integer> q = new LinkedList<>();
+        return dfs(source, destination, adj, visited);
+    }
 
-        q.add(source);
-        visited[source] = true;
+    private boolean dfs(int node, int dest,ArrayList<ArrayList<Integer>> adj,boolean[] visited) {
 
-        while (!q.isEmpty()) {
-            int curr = q.poll();
-              if (curr == destination) return true;
+        if (node == dest) return true;   
+        visited[node] = true;
 
-            for (int neighbour : adj.get(curr)) {
-                if (!visited[neighbour]) {
-                    visited[neighbour] = true;
-                    q.add(neighbour);
+        for (int neighbour : adj.get(node)) {
+            if (!visited[neighbour]) {
+                if (dfs(neighbour, dest, adj, visited)) {
+                    return true; 
                 }
             }
-        
         }
-        return false;
-    }
+        return false; 
+    }    
 }
