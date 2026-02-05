aa2c05b23fa2821b0ddbd0115ffad1c070c412a3
PriyanshiRaina07 <priyanshiraina07@gmail.com>
2026-02-06 00:15:03 +0530

    Time: 3 ms (43.82%), Space: 46.5 MB (74.37%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -0,0 +1,45 @@
+class Solution {
+    static int[] dr = {-1,1,0,0};
+    static int[] dc = {0,0,-1,1};
+    static int rowL;
+    static int colL;
+    public int closedIsland(int[][] grid) {
+        rowL=grid.length;
+        colL = grid[0].length;
+
+        boolean[][] visited = new boolean[rowL][colL];
+
+        for(int i = 0; i < rowL ; i++){
+            for(int j = 0; j <colL; j++){
+                if(i == 0|| j == 0|| i == rowL - 1|| j == colL - 1&&grid[i][j] == 0){
+                    dfs(i,j,visited,grid);
+                }
+            }
+        }
+         int count = 0;
+        for(int i = 0; i < rowL; i++){
+            for(int j = 0; j < colL; j++){
+                if(!visited[i][j] && grid[i][j] == 0){
+                    dfs(i, j, visited, grid);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    
+    }
+     private void dfs(int r, int c, boolean[][] visited, int[][] grid){
+        if(r < 0 || r >= rowL || c < 0 || c >= colL || grid[r][c] == 1)
+            return;
+
+        if(visited[r][c]) 
+            return;
+
+        visited[r][c] = true;
+
+        for(int i = 0; i < 4; i++){
+            dfs(r + dr[i], c + dc[i], visited, grid);
+        }
+    }    
+}
