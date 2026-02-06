24cdf92087e5d04077cef7f0ea091c0a918c3674
PriyanshiRaina07 <priyanshiraina07@gmail.com>
2026-02-06 11:28:24 +0530

    Time: 6 ms (23.4%), Space: 51.3 MB (91.23%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,33 +1,53 @@
-class Solution {
+ class Solution {
     static int []dr = {-1,1,0,0};
     static int []dc = {0,0,-1,1};
     static int rowL;
     static int colL;
+    static char []direction = {'U','D','L','R'};
+    static class pair{
+        int row;
+        int col;
+        public pair(int row, int col){
+            this.row = row;
+            this.col = col;
+        }
+    }
     public int numIslands(char[][] grid) {
         rowL = grid.length;
         colL = grid[0].length;
         
         boolean [][]visited = new boolean[rowL][colL];
         int component = 0;
-
         for(int i = 0; i < rowL; i++){
-            for(int j = 0; j < colL; j ++){
+            for(int j = 0; j < colL; j++){
                 if(!visited[i][j] && grid[i][j] == '1'){
-                    dfs(i,j,grid,visited);
+                    bfs(i,j,visited,grid);
                     component++;
-                }
+                }    
             }
         }
         return component;
     }
-    public void dfs(int r, int c, char[][]grid,boolean[][] visited){
-        if(r < 0|| c < 0 || r >= rowL|| c >= colL|| grid[r][c] == '0' || visited[r][c]){
-            return;
-        }
+    public void bfs(int r, int c, boolean[][] visited, char[][]grid){
+        Queue<pair>q = new LinkedList<>();
+
         visited[r][c] = true;
-      
-        for(int i = 0; i < 4; i++){
-            dfs(r + dr[i], c + dc[i], grid, visited);
+        q.offer(new pair(r,c));
+
+        while(!q.isEmpty()){
+            pair curr = q.poll();
+            int R = curr.row;
+            int C = curr.col;
+            for(int i = 0; i < 4; i++){
+                int nRow = R + dr[i];
+                int nCol = C +dc[i];
+                if(nRow >= 0 && nCol >= 0 && nRow < rowL && nCol < colL && !visited[nRow][nCol] && grid[nRow][nCol] =='1'){
+                    visited[nRow][nCol] = true;
+                    q.offer(new pair(nRow,nCol));
+                }
+            }
         }
     }
-}
+}  
+       
+   
